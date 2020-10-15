4565d502c3a0b13928f409f18a7c753acb9d2c4c
Bhumin Bhandari <bhandaribhumin@gmail.com>
2020-10-15 15:25:29 +0530

    set screen size

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -13,6 +13,7 @@ export class FilterComponent implements OnInit {
   isActivYear:string = "";
   isActivLaunch:boolean;
   isActivLandLaunch:boolean;
+  storeFilter:string = '';
   constructor(private _spacexService: SpaceXService) { }
 
   launchYears = [2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020];
@@ -25,18 +26,21 @@ this.filterQueryParams = {} as IFilter;
    this.isActivYear = event.target.value;
    this.filterQueryParams.launchYear = event.target.value;
    this._spacexService.getSpaceXLaunches(this.filterQueryParams).subscribe();
+   this.storeFilter = `/${event.target.value}`;
   }
 
   filterAsPerSuccessLaunch(value: boolean){
     this.isActivLaunch = value;
     this.filterQueryParams.launchSuccess = value;
     this._spacexService.getSpaceXLaunches(this.filterQueryParams).subscribe();
+    this.storeFilter = this.storeFilter + `/${value}`;
   }
 
   filterAsPerLandLaunch(value: boolean) {
     this.isActivLandLaunch = value;
     this.filterQueryParams.landSuccess = value;
     this._spacexService.getSpaceXLaunches(this.filterQueryParams).subscribe();
+    this.storeFilter = this.storeFilter + `/${value}`;
   }
-
+  
 }
